77efe0bffd1ae53b2c431fb29fc16f494eca2403
Gustavo M T Borges <gustavo.mtborges@gmail.com>
2018-03-20 17:10:49 -0300

    Spring boot project structure

---
+++ src/main/groovy/br/com/goifnra/swarm/SwarmApplication.groovy
@@ -0,0 +1,12 @@
+package br.com.goifnra.swarm
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SwarmApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run SwarmApplication, args
+	}
+}
---
+++ src/test/groovy/br/com/goifnra/swarm/SwarmApplicationTests.groovy
@@ -0,0 +1,16 @@
+package br.com.goifnra.swarm
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class SwarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
